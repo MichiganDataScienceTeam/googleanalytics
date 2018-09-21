ae4b36069c7711009143081f5880a3e76c93f064
Jonathan Stroud <stroud@umich.edu>
2018-09-21 18:23:14 -0400

    pep8

--- dataset.py
+++ dataset.py
@@ -13,37 +13,43 @@ class Dataset():
     def __init__(self, debug=False):
         """Load the data from disk.
 
-        Args: 
-            debug (bool): An option to choose whether to load all data. 
-                          If 'debug' is true, program will only read 100 rows data from the csv file.
-        
+        Args:
+            debug (bool): An option to choose whether to load all
+              data.  If 'debug' is true, program will only read 100 rows
+              data from the csv file.
         """
 
-
-        if(debug): nrows = 100
-        else:      nrows = None
+        if(debug):
+            nrows = 100
+        else:
+            nrows = None
         type_change_columns = {"fullVisitorId": str,
-                        "sessionId": str,
-                        "visitId": str}
+                               "sessionId": str,
+                               "visitId": str}
         json_columns = ['device', 'geoNetwork', 'totals', 'trafficSource']
         self.train = pd.read_csv(os.path.join(_DATA_DIR, _TRAIN),
-                                 converters={column: json.loads for column in json_columns},  
+                                 converters={
+                                     column: json.loads
+                                     for column in json_columns},
                                  dtype=type_change_columns,
-                                 nrows = nrows)
-                                
-        self.test = pd.read_csv(os.path.join(_DATA_DIR, _TEST), 
-                                converters={column: json.loads for column in json_columns},
+                                 nrows=nrows)
+
+        self.test = pd.read_csv(os.path.join(_DATA_DIR, _TEST),
+                                converters={
+                                    column: json.loads
+                                    for column in json_columns},
                                 dtype=type_change_columns,
-                                nrows = nrows)
+                                nrows=nrows)
         for column in json_columns:
             train_column_as_df = pd.io.json.json_normalize(self.train[column])
             test_column_as_df = pd.io.json.json_normalize(self.test[column])
-            self.train = self.train.merge(train_column_as_df, 
-                                        right_index=True, 
-                                        left_index=True)                   
-            self.test = self.test.merge(test_column_as_df, 
-                                        right_index=True, 
+            self.train = self.train.merge(train_column_as_df,
+                                          right_index=True,
+                                          left_index=True)
+            self.test = self.test.merge(test_column_as_df,
+                                        right_index=True,
                                         left_index=True)
+
 
 if __name__ == '__main__':
 
@@ -53,7 +59,7 @@ if __name__ == '__main__':
     # Sanity check, make sure we have the right number of rows
     num_train = len(dataset.train)
     num_test = len(dataset.test)
-    assert num_train == 100, 'Incorrect number of training examples.' # 903653
-    assert num_test == 100, 'Incorrect number of test examples.' # 804684
+    assert num_train == 100, 'Incorrect number of training examples.'  # 903653
+    assert num_test == 100, 'Incorrect number of test examples.'  # 804684
 
     print('Successfully loaded the dataset.')
