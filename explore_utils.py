97a94f44cbc69ef8ac3475ddb7b752418357ba7e
Jonathan Stroud <stroud@umich.edu>
2018-09-27 15:38:47 -0400

    compute percentiles of total revenue per customer

--- explore_utils.py
+++ explore_utils.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def find_most_visit(dataset):
     """Find what is the most visited times of single customer.(only in train set)
 
@@ -15,3 +18,29 @@ def find_most_visit(dataset):
     max_visit = train_gdf['visitNumber'].max()
 
     return max_visit
+
+
+def find_customer_revenue_percentiles(
+        dataset,
+        percentiles=[95, 99, 99.9, 99.99]):
+    """Find percentiles of the per-customer revenue.
+
+    args:
+       dataset (Dataset): the google analytics dataset
+       percentiles (list of floats): the percentiles to find
+
+    returns:
+       The values of the per-customer revenue at the given
+       percentiles in the training set.
+
+    """
+
+    train_df = dataset.train
+    train_df['revenue'] = train_df['totals.transactionRevenue'].astype(float)
+    revenue_per_customer = train_df.groupby('fullVisitorId')['revenue']
+    total_revenue_per_customer = revenue_per_customer.sum().fillna(0) / 10000.0
+
+    values = [np.percentile(total_revenue_per_customer.values, percentile)
+              for percentile in percentiles]
+
+    return values
