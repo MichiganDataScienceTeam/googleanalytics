ba356aa8905ab35dffa1c37beb82ba4b110277cb
Jonathan Stroud <stroud@umich.edu>
2018-09-27 09:55:36 -0400

    bugfix in explore

--- explore.py
+++ explore.py
@@ -1,19 +1,21 @@
+from __future__ import print_function
+
 import dataset
 import explore_utils
 
-
 def main():
 
+    print("Loading the dataset...")
     data = dataset.Dataset()
+    
+    print("Number of rows in the training set:    ", len(data.train))
+    print("Number of columns in the training set: ", len(data.train.columns))
 
-    print("Training set")
-    print(data.train.describe())
-
-    print("Test set")
-    print(data.test.describe())
-
+    print("Number of rows in the test set:        ", len(data.test))
+    print("Number of columns in the test set:     ", len(data.test.columns))
+    
     print("The most visit times for a customer in train set is: ",
-          explore_utils.find_most_visit(dataset))
+          explore_utils.find_most_visit(data))
 
 
 if __name__ == '__main__':
