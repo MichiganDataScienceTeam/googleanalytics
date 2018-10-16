826f7301949eecf5ff306d242960a9dff86c4b5b
Shengyu Feng <38071175+Shengyu-Feng@users.noreply.github.com>
2018-10-16 13:48:49 -0400

    Average Revenue Per Traffic Source (#54)
    * Average Revenue Per Traffic Source
    
    * Average Revenue Per Traffic Source
    
    * Average Revenue Per Traffic Source
    
    * Revenue per traffic source

--- explore.py
+++ explore.py
@@ -29,12 +29,14 @@ def main(args):
         percentiles)
     for p, pv in zip(percentiles, percentile_values):
         print("%2.2f%% of customers spend less than: $%.2f" % (p, pv))
-    
+
+    # Total average revenue per trafficSource 
+    print("The revenue per trafficSource is: ")
+    print(explore_utils.revenue_per_trafficsource(data))
 
     # The fraction of transactions that have non-zero revenue
     print("Fraction of transactions that have non-zero revenue:    ",
           explore_utils.find_fraction_of_transactions_with_non_zero_revenue(data))
-    
 
     # Most common Sources of Traffic and counts
     num_of_sources = 6
